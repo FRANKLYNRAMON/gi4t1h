7493f212ee7627b51054945e8072bd28e50733f0
FRANKLYNRAMON <112025613+FRANKLYNRAMON@users.noreply.github.com>
2023-05-12 16:03:43 -0300

    5 files modified, 8 added

--- src/app/cart.service.spec.ts
+++ src/app/cart.service.spec.ts
@@ -1,6 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CartService } from './cart.service';
+//import { CartService } from './cart.service';
+import { Product } from './products';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: Product[] = [];
+
+  addToCart(product: Product) {
+    this.items.push(product)
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+};
 
 describe('CartService', () => {
   let service: CartService;
